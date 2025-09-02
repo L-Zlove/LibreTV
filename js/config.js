834ce39e7409381f127469f27652b352a7a2c163
L-Zlove <96755703+L-Zlove@users.noreply.github.com>
2025-09-02 13:59:39 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -121,16 +121,6 @@ const API_SITES = {
         api: 'https://api.niuniuzy.me/api.php/provide/vod',
         name: '牛牛资源',
         adult: false
-    },
-        hsck: {
-        api: 'https://hsckzy888.com/api.php/provide/vod',
-        name: '黄色仓库',
-        adult: false
-    },
-        91: {
-        api: 'https://91jpzyw.com/api.php/provide/vod',
-        name: '91资源',
-        adult: false
     }
 
 };
